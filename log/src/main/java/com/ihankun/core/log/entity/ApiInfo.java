f1d262aff725e9f10c7fb1872000dbd9061da064
hankun <ihankun@outlook.com>
2023-05-27 16:26:08 +0800

    hankun-引入logback

---
+++ log/src/main/java/com/ihankun/core/log/entity/ApiInfo.java
@@ -0,0 +1,30 @@
+package com.ihankun.core.log.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class ApiInfo implements Serializable {
+
+    private static final long serialVersionUID = 8331334991539998037L;
+
+    /**
+     * api的目标服务名称
+     */
+    private String targetServiceName;
+
+    /**
+     * 目标服务的url地址
+     */
+    private String targetUrl;
+
+
+    public ApiInfo() {
+    }
+
+    public ApiInfo(String targetServiceName, String targetUrl) {
+        this.targetServiceName = targetServiceName;
+        this.targetUrl = targetUrl;
+    }
+}
